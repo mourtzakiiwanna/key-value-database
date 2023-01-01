0eaee67ca899a46a8adb163d6188249b1ae97545
Ioanna Mourtzaki <109597293+mourtzakiiwanna@users.noreply.github.com>
2023-01-01 17:51:12 +0200

    Add files via upload

--- genData.py
+++ genData.py
@@ -3,12 +3,58 @@ import sys
 import random
 import getopt
 
-# Returns the error message
+def main(argv):
+    
+    opts, _ = getopt.getopt(argv, "hk:n:d:l:m:")
+
+    # checking user input and throw error message if any argyment is missing 
+    if len(opts) != 5:
+        create_error_message()
+        sys.exit()
+
+    # initializing the variables with the values given from the user 
+    for opt, arg in opts:
+        if opt in ("-k"):
+            key_file = arg
+        elif opt in ("-n"):
+            lines = int(arg)
+        elif opt in ("-d"):
+            nesting_level = int(arg)
+        elif opt in ("-l"):
+            max_string_length = int(arg)
+        elif opt in ("-m"):
+            max_keys = int(arg)
+
+    # reading the 'keyFile.txt', parsing the key names/types and add them on 'keys' list 
+    # 'keys' list stores the name and the type of each key [[name,string],[age,int]..]
+    file = open(key_file, 'r')
+    keys = []
+    for i in file.readlines():
+        keys.append(i.split())
+
+    # generating random data and store them in 'payload' dict 
+    # setting high-level keys as key1, key2..
+    payload = {}
+    for i in range(1,lines):
+        payload['\'key' + str(i)+'\''] = create_payload(keys,nesting_level, max_keys, max_string_length)
+
+    # writing generated data to 'dataToIndex' file
+    outputFile = 'dataToIndex.txt'
+    f = open(outputFile, "w")
+    
+    for key in payload:
+        f.write(key + " : " + str(payload[key]) + "\n")
+
+    print("\nData was successfully generated.")
+
+
+# returns an error message when any argument is missing from user's input 
 def create_error_message():
     print("\nSome arguments are missing. Please provide the required info in this order: genData.py -k -n -d -l -m , where : \n\n -k is a file containing a space-separated list of key names and their data types, \n -n indicates the number of lines that you would like to generate, \n -d is the maximum level of nesting, \n -m is the maximum number of keys inside each value, \n -l is the maximum length of a string value whenever you need to generate a string")
 
-# Returns a random value of specific length based on key type
-def create_random_value(max_string_length,type):
+# returns a random value of specific length based on key type
+# exaple: if max_string_length is 4 -> String 'Qvmq', int '6574', float '1202.93'
+def create_random_value(max_string_length, type):
 
     max_length = int(max_string_length)
     min = pow(10, max_length-1)
@@ -23,94 +69,47 @@ def create_random_value(max_string_length,type):
 
     return str(random_value)
 
-# Returns the payload generated for every high-level key (key0,key1..)
+# returns the payload generated for every high-level key (key1,key2..)
 def create_payload(keyTypes, nesting, max_keys, max_string_length):
 
-    indexes = []
+    # this list will store the keys we have already used in order not to use them twice 
+    keys_used = []
+    
     # randomly choose the max key number for this high-level key 
-    rand_max_key = random.randint(0, max_keys)
+    inside_keys = random.randint(0, max_keys)
     payload = {}
 
-    # loop up to max key and create payload 
-    for _ in range(0, rand_max_key):
+    # loop up to max key and create payload (using recursion)
+    for i in range(0, inside_keys):
 
-        index = random.randint(0, max_keys-1)
+        key = random.randint(0, max_keys-1)
 
-        if index not in indexes:
-            indexes.append(index)
-        elif (len(indexes) != max_keys):
-            while index in indexes:
-                index = random.randint(0, max_keys-1)
-        else:
+        # to be sure that the same key is not appeared twice in the same level 
+        if (len(keys_used) != max_keys):
+            if key not in keys_used:
+                keys_used.append(key)
+            else: 
+                while key in keys_used:
+                    key = random.randint(0, max_keys-1)
+        else: 
             continue
 
-        # if the nesting level is not 0, we randomly choose if we will apply the nest or not 
+        nesting_level = 0
+        
+        # if the nesting level is not 0, we randomly choose the nesting level we will apply 
         if nesting > 0:
-            isNesting = random.choice([True, False])
+            nesting_level = int(round(random.random()*nesting, 0))
 
         # if we will apply nest, we recursively call the 'create_payload' reducing every time the nesting level 
-        if(nesting > 0 and isNesting):
-            payload[keyTypes[index][0]] = create_payload(keyTypes, nesting - 1, max_keys, max_string_length)
+        if(nesting_level > 0):
+            payload[keyTypes[key][0]] = create_payload(keyTypes, nesting_level - 1, max_keys, max_string_length)
         # else if we won't apply nesting, we just fill in with a random key value based on key type 
         else:
-            type = keyTypes[index][1]
+            type = keyTypes[key][1]
             value = create_random_value(max_string_length,type)
-            payload[keyTypes[index][0]] = value
+            payload[keyTypes[key][0]] = value
 
     return payload
-
-
-def main(argv):
-    
-    opts, _ = getopt.getopt(argv, "hk:n:d:l:m:")
-
-    if len(opts) != 5:
-        create_error_message()
-        sys.exit(2)
-
-    # initializing the variables with the values given from the user 
-    for opt, arg in opts:
-        if opt in ("-k"):
-            key_file = arg
-        elif opt in ("-n"):
-            lines = int(arg)
-        elif opt in ("-d"):
-            nesting_level = int(arg)
-        elif opt in ("-m"):
-            max_keys = int(arg)
-        elif opt in ("-l"):
-            max_string_length = int(arg)
-
-    # 'keys' list have the name and the type of each key [[name,string],[age,int]]
-    file = open(key_file, 'r')
-    keys = []
-    for i in file.readlines():
-        keys.append(i.split())
-
-    # payload generation
-    payload = {}
-    for j in range(lines):
-        payload['\'key' + str(j)+'\''] = create_payload(keys,nesting_level, max_keys, max_string_length)
-
-    # write generated data to file
-    with open("dataToIndex.txt", 'w') as f:
-        for d in payload:
-            f.write(d + ":" + str(payload[d]))
-            f.write('\n')
-    
-    """
-
-    with open("dataToIndex.txt", 'r') as f:
-        
-        filedata = f.read()
-
-    filedata = filedata.replace(':','->')
-    filedata = filedata.replace(',','|')
-
-    with open("dataToIndex.txt", 'w') as f:
-        f.write(filedata)
-
-    """
 
 if __name__ == "__main__":
     main(sys.argv[1:])
